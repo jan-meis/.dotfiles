c6f1b2b87b7c481ebe91fa380a3a95a8ea5179a6
Jan Meis <jan.meis@outlook.com>
2024-10-06 11:11:43 +0200

    update

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,10 +1,16 @@
 local wezterm = require 'wezterm'
-
 -- This will hold the configuration.
 local config = wezterm.config_builder()
+config.hide_tab_bar_if_only_one_tab = false
 config.font = wezterm.font {
-    family = 'IosevkaTerm NF',
+    family = 'IosevkaTerm Nerd Font',
     weight = 'Regular'
+}
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
 config.font_size = 16
 config.window_decorations = "RESIZE"
