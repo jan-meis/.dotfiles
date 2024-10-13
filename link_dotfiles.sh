f39bdf5f47b026ff81af37460488cf73c0645adc
Jan Meis <jan.meis@outlook.com>
2024-10-13 11:00:35 +0200

    link_dotfiles.sh

---
+++ link_dotfiles.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+readarray -t tracked < $HOME/.dotfiles/tracked
+
+for path in "${tracked[@]}"
+do
+    mkdir -p "$HOME/$(dirname ${path})"
+    ln -s  "$HOME/.dotfiles/${path}" "$HOME/${path}"
+done
