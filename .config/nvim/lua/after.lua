a6564ce07e4445c1c03596751ca0c2a61b135ba4
Jan Meis <jan.meis@sap.com>
2024-10-15 09:46:51 +0200

    undodir

--- .config/nvim/lua/after.lua
+++ .config/nvim/lua/after.lua
@@ -10,7 +10,7 @@ vim.opt.smartindent = true
 vim.opt.wrap = false
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv("UNDODIR") .. "/.vim/undodir"
 vim.opt.undofile = true
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
@@ -118,7 +118,6 @@ require("lspconfig").clangd.setup {
     "--fallback-style=llvm"
   }
 }
-
 
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
